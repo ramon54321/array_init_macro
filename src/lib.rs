4741aec6584e9273779f1a9506dd2dfc18a64bef
Ramon Brand <ramon2009@gmail.com>
2022-11-30 12:26:43 +0200

    Change literal count to expression

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 #[macro_export]
 macro_rules! arr {
-    ($type:ty; $count:literal; $init:expr) => {
+    ($type:ty; $count:expr; $init:expr) => {
         unsafe {
             let mut array: [std::mem::MaybeUninit<$type>; $count] =
                 std::mem::MaybeUninit::uninit_array();
@@ -26,5 +26,9 @@ mod tests {
         assert_eq!(array0[1], 1);
         assert_eq!(array0[2], 1);
         assert_eq!(array0[3], 1);
+
+        const COUNT: usize = 3;
+        let array1 = arr![u8; COUNT; 5];
+        assert_eq!(array1.len(), COUNT);
     }
 }
